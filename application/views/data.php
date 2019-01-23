1ba0f94a3627f838d9d98b8f40d0349d044a1df6
Elena <patrascuelena09@gmail.com>
2019-01-24 01:16:53 +0200

    no message

---
+++ application/views/data.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <title>Welcome!</title>
+
+</head>
+
+<body style="background-image:url('https://images4.alphacoders.com/861/861292.jpg');">
+
+<center><h1 style="font-size:70px;">Welcome!</h1></center>
+
+</body>
+</html>
